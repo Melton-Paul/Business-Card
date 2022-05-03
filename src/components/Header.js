4d7fb398cbdb065decb3120cbf86dc554116b12e
Paul Melton <melton.paul@hotmail.com>
2022-05-03 17:01:29 -0500

    Started React, built template, built header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <header>
+            <h1 className="header--title">Paul Melton</h1>
+            <p className="header--job__title">Frontend Developer</p>
+            <a className="header--website" href="https://movelistcreator.netlify.app/">PaulMelton.com</a>
+        </header>
+    )
+}
